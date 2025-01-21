d31852c1ef3e5d7c30841f26f71b4c087bee6dcc
Jose Rondon <joserondon0319@gmail.com>
2025-01-21 17:17:11 -0500

    Creacion del ejercicio6.py

--- ejercicio3.py
+++
@@ -1,8 +0,0 @@
-#Escribir un programa que convierta un valor dado en grados Fahrenheit a grados Celsius. Recordar que la fórmula para la conversión es: C = (F-32)*5/9
-print('Conversor de Temperatura de grados Fahrenheit a grados Celsius\n')
-
-fahrenheit = float(input('Ingrese la temperatura en grados Fahrenheit: '))
-
-celsius = (fahrenheit-32)*5/9
-
-print(f'La temperatura en grados Celsius es: {celsius:.2f}')
